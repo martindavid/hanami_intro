baa7d95f9752cec389bf0fd22ba00fea68d70d53
Martin David Valentino <martin.valentino@cultureamp.com>
2020-07-04 20:17:03 +1000

    initial commit

---
+++ apps/web/controllers/home/index.rb
@@ -0,0 +1,14 @@
+module Web
+  module Controllers
+    module Home
+      class Index
+        include Web::Action
+
+        def call(params)
+          params
+        end
+      end
+
+    end
+  end
+end
